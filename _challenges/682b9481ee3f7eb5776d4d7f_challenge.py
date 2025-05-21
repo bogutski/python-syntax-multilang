cf8e9e8e8e11b0e3c58841cbb2c10cd37fa4c222
Andrew1326 <andreimanzhola@gmail.com>
2025-05-21 13:51:22 +0100

    course data

---
+++ _challenges/682b9481ee3f7eb5776d4d7f_challenge.py
@@ -0,0 +1,51 @@
+// NAMEEN:Print all words in a sentence in reverse order
+// NAMERU:Вывести все слова в предложении в обратном порядке
+
+/*
+INSTRUCTIONENSTART
+Instructions EN
+INSTRUCTIONENEND
+*/
+
+/*
+INSTRUCTIONRUSTART
+Instructions RU
+INSTRUCTIONRUEND
+*/
+
+/*
+HINTENSTART
+Hint EN
+HINTENEND
+*/
+
+/*
+HINTRUSTART
+Hint RU
+HINTRUEND
+*/
+
+/*
+INITIALSOLUTIONSTART
+def reverse_words(arr):
+INITIALSOLUTIONEND
+*/
+
+// SOLUTIONSTART
+def reverse_words(arr):
+     tmp = arr.split(" ")
+     return " ".join(tmp[::-1])
+// SOLUTIONEND
+
+
+// OPENTESTSSTART
+def test_reverse_words_1():
+       assert (reverse_words('hello localcoding') == 'localcoding hello')
+
+   def test_reverse_words_2():
+       assert (reverse_words('what we will do in the templates') == 'templates the in do will we what')
+// OPENTESTSEND
+
+// HIDDENTESTSSTART
+
+// HIDDENTESTSEND
