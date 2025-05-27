7d1c1879bd8a3ab6a7cd310df5168760d723442f
Aleksandr Romas <romas.0121@gmail.com>
2025-05-27 19:16:58 -0400

    Add: First program Hello World

---
+++ _challenges/6696c2ca763f0712c198daf6_challenge.js
@@ -0,0 +1,76 @@
+// NAMEEN:First program "Hello World"
+// NAMERU:Первая программа "Hello world"
+
+/*
+INSTRUCTIONENSTART
+![](https://course-qa-basics.s3.us-west-1.amazonaws.com/section-1-course1-task-1.png)
+Output the phrase "Hello world" to the console.
+
+To do this, type the command:
+```
+print("Hello world")
+```
+After that, click the - `Validate solution` button.
+
+If error messages appear in the window on the right, correct them.
+
+If the green mark `passed` appears in the window on the right, then you have done everything correctly.
+
+Then click the `Submit solution` button.
+
+The task is considered completed when a green marking `Completed` will appear next to the task name.
+INSTRUCTIONENEND
+*/
+
+/*
+INSTRUCTIONRUSTART
+![](https://course-qa-basics.s3.us-west-1.amazonaws.com/section-1-course1-task-1.png)
+
+Выведите в консоль фразу "Hello world".
+
+Для этого напечатайте команду:
+```
+print("Hello world")
+```
+После этого нажмите кнопку - `Проверить решение`.
+
+Если в появившемся справа окне появились сообщения об ошибках, исправьте их.
+
+Если в окне справа появилась зеленая отметка `passed`, значит, вы все сделали правильно.
+
+После этого нажмите кнопку - `Отправить решение`.
+
+При этом задание считается выполненным, если около названия задачи появится зеленая отметка - `Completed`.
+INSTRUCTIONRUEND
+*/
+
+/*
+HINTENSTART
+
+HINTENEND
+*/
+
+/*
+HINTRUSTART
+
+HINTRUEND
+*/
+
+/*
+INITIALSOLUTIONSTART
+
+INITIALSOLUTIONEND
+*/
+
+// SOLUTIONSTART
+print("Hello world")
+// SOLUTIONEND
+
+
+// OPENTESTSSTART
+print("Hello world")
+// OPENTESTSEND
+
+// HIDDENTESTSSTART
+
+// HIDDENTESTSEND
