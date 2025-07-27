754d7efe4366e3f49f1dda9367fa7642388cf97c
Alexander Romas <117665497+Aleksandr-Romas@users.noreply.github.com>
2025-07-27 17:56:11 -0400

    Add quiz (#18)
    * Add wonders of assignment
    
    * Add quiz

---
+++ _challenges/6696c2ca763f0712c198dbf1_challenge.py
@@ -0,0 +1,68 @@
+// NAMEEN:Increase by 5
+// NAMERU:Увеличение на 5
+
+/*
+INSTRUCTIONENSTART
+Create a variable `count` and assign it the value `20`.
+Then increment the existing value of the `count` variable by `5`.
+To do this, write the command:
+
+```python
+count = 20
+count = count + 5
+```
+INSTRUCTIONENEND
+*/
+
+/*
+INSTRUCTIONRUSTART
+Создайте переменную `count` и присвойте ей значение `20`:
+
+```python
+count = 20
+```
+
+Затем запишите команду, увеличивающую имеющееся значение переменной count на `5`:
+```python
+count = count + 5
+```
+
+Итого, в итоге должно получиться так:
+```python
+count = 20
+count = count + 5
+```
+INSTRUCTIONRUEND
+*/
+
+/*
+HINTENSTART
+
+HINTENEND
+*/
+
+/*
+HINTRUSTART
+
+HINTRUEND
+*/
+
+/*
+INITIALSOLUTIONSTART
+
+INITIALSOLUTIONEND
+*/
+
+// SOLUTIONSTART
+count = 20
+count = count + 5
+// SOLUTIONEND
+
+// OPENTESTSSTART
+
+// OPENTESTSEND
+
+// HIDDENTESTSSTART
+count = 20
+count = count + 5
+// HIDDENTESTSEND
