98b033d0e554637f1155044d283ff2bf6182be8e
Aleksandr Romas <romas.0121@gmail.com>
2025-08-11 23:35:59 -0400

    Add challenges

---
+++ _challenges/6696c2ca763f0712c198dc1a_challenge.py
@@ -0,0 +1,50 @@
+// NAMEEN:Type of integer number
+// NAMERU:Тип целого числа
+
+/*
+INSTRUCTIONENSTART
+Create a variable `type_of_integer` and assign it the type of the integer number `55`.
+INSTRUCTIONENEND
+*/
+
+/*
+INSTRUCTIONRUSTART
+Создайте переменную `type_of_integer` и присвойте ей тип числа `55`.
+INSTRUCTIONRUEND
+*/
+
+/*
+HINTENSTART
+Similar example:
+```python
+type_of_phone_number = type(911)
+```
+HINTENEND
+*/
+
+/*
+HINTRUSTART
+Похожий пример:
+```python
+type_of_phone_number = type(911)
+```
+HINTRUEND
+*/
+
+/*
+INITIALSOLUTIONSTART
+
+INITIALSOLUTIONEND
+*/
+
+// SOLUTIONSTART
+type_of_integer = type(age)
+// SOLUTIONEND
+
+// OPENTESTSSTART
+
+// OPENTESTSEND
+
+// HIDDENTESTSSTART
+type_of_integer = type(age)
+// HIDDENTESTSEND
